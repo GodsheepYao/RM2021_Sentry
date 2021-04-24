bd17612316265240950334f606bb72204478b139
LX050724 <yaojizhong@outlook.com>
2021-04-24 08:59:00 +0800

    初步完成云台运行时代码框架

--- SentryDown/USER/Src/FreeRTOS_Task.c
+++ SentryDown/USER/Src/FreeRTOS_Task.c
@@ -1,33 +1,31 @@
 #include "FreeRTOS_Task.h"
 
-//Pitch轴、Yaw轴电机数据
-GM6020_TypeDef GM6020_Pitch,GM6020_Yaw;
+#include "PTZ_Init_Task.h"
 
-//拨弹电机数据
+GM6020_TypeDef GM6020_Pitch, GM6020_Yaw;
+
 M2006_TypeDef Pluck1;
 
-//摩擦轮电机数据
 RM3510_TypeDef Frictionwheel1,Frictionwheel2;
 
-//Pitch轴角度、速度PID
 PID_Smis GM6020_Pitch_PID = {.Kp = 0,.Ki = 0,.Kd = 0,.limit = 5000};
 PID GM6020_Pitch_SPID = {.Kp = 0,.Ki = 0,.Kd = 0};
 
-//Yaw轴角度、速度PID
 PID_Smis GM6020_Yaw_PID = {.Kp = 0,.Ki = 0,.Kd = 0,.limit = 5000};
 PID GM6020_Yaw_SPID = {.Kp = 0,.Ki = 0,.Kd = 0};
 
-//拨弹电机速度PID
 PID Pluck1_SPID = {.Kp = 0,.Ki = 0,.Kd = 0,.limit = 5000};
 
-//摩擦轮速度PID
 PID Frictionwheel1_SPID = {.Kp = 0,.Ki = 0,.Kd = 0,.limit = 1000};
 PID Frictionwheel2_SPID = {.Kp = 0,.Ki = 0,.Kd = 0,.limit = 1000};
 
+Robot_Status_t Robot_Status;
+float Yaw_offset;
+PTZAngle_Ref_t PTZAngle_Ref = {.Pitch = PTZ_Pitch_median,.Yaw = 0};
+
 
 /*初始任务*/
-void StartTask(void)
-{   
+void StartTask(void) {   
 	CanFilter_Init(&hcan1);
 	HAL_CAN_Start(&hcan1);
 	HAL_CAN_ActivateNotification(&hcan1, CAN_IT_RX_FIFO0_MSG_PENDING);
@@ -42,8 +40,7 @@ void StartTask(void)
                 (TaskHandle_t *)&PTZ_Init_Handler);
 }
 
-void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan)
-{
+void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan) {
   if (hcan->Instance == CAN1) {
     uint32_t can_id = CAN_Receive_DataFrame(&hcan1, CAN1_buff);
     switch (can_id) {
@@ -66,8 +63,7 @@ void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan)
   }
 }
 
-uint32_t Get_TimerTick()
-{
+uint32_t Get_TimerTick() {
     return TIM2->CNT;
 }
 
